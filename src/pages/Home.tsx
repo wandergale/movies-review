93425d0f702a834fea0b0bdb5c9a68398b97e1c2
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-04 14:21:27 -0300

    connecting api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
   return (
     <div>
